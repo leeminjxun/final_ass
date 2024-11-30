6bfe181288178a70204b4b4745ba967811c2031b
이민준 <th554751@naver.com>
2024-12-01 04:13:32 +0900

    UI 개선

--- bus_reservation/settings.py
+++ bus_reservation/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'bus_reservation' / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'bus_reservation' / 'static']
 
 
 # Default primary key field type
